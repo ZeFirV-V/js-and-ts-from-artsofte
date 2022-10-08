6128947a55c67e936c69ed3b56aae5a0cd66c4f0
Сахабутдинов Ильфир <ilfir.sakhabutdinov@bk.ru>
2022-10-08 18:49:57 +0500

    Решение 2 номера из 3 модуля заданий

---
+++ js-3-main/task-2/solution.js
@@ -0,0 +1,28 @@
+export function lock(sourceObj, keys){
+    if (typeof sourceObj !== 'object' || sourceObj === null){
+        throw new Error('Uncorrect source object')
+    }
+
+    if (typeof keys === 'undefined' || keys.length === 0 || !keys.every(i => typeof i === "string")){
+        throw new Error('Wrong keys argument')
+    }
+
+    let a = Object.keys(sourceObj);
+    for (let key of keys) {
+        if (key.indexOf('.') === -1 && a.indexOf(key) <= -1) {
+            throw new Error('NoneKeyError')
+        }
+    }
+
+    for (let key of keys){
+        if (key.indexOf('.') > -1 && typeof sourceObj[key.split(".")[0]] === "object"){
+            lock(sourceObj[key.split(".")[0]], [key.substring(key.indexOf('.') + 1)])
+        }
+        else{
+            Object.defineProperty(sourceObj, [key], {
+                writable: false,
+                configurable: false
+            })
+        }
+    }
+}
